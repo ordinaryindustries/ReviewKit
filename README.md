efeb4b2a8048fa3b800db84389b7d5faabb74d57
Ordinary Industries <132616209+ordinaryindustries@users.noreply.github.com>
2024-02-04 23:11:57 -0500

    Add steps for layout

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ var body: some View {
     }
 }
 ```
-<img width="510" alt="image" src="https://github.com/ordinaryindustries/ReviewKit/assets/132616209/41bf9069-2b3d-468d-b7fb-bb069371da29">
-
-
-
-The default behavior is to display the star rating as well as the rating score. To disable the score you can pass `showScore: false` to the `ShapeProgressView`.
+The default behavior is to display the star rating as well as the rating score. When initalizing `ShapeProgressView` you can set the layout type with an optional property `layout`. Options are .automatic, .score, and .graphical.
 ```
-ShapeProgressView(value: $value, showScore: false)
+ShapeProgressView(value: $value, layout: .score)
 ```
-<img width="513" alt="image" src="https://github.com/ordinaryindustries/ReviewKit/assets/132616209/c307577a-36f7-48a3-a84e-0456daf7f44e">
+
+.automatic
+
+.score
+
+.graphical
--- Sources/ReviewKit/ReviewKit.swift
+++ Sources/ReviewKit/ReviewKit.swift
@@ -11,33 +11,48 @@
 
 import SwiftUI
 
+public enum LayoutType {
+    case full, score, graphical
+}
+
 public struct ShapeProgressView: View {
     let count: Int
     @Binding var value: Double
     let imageName: String
     let color: Color
-    let showScore: Bool
+    let layout: LayoutType
     
-    public init(count: Int = 5, value: Binding<Double>, imageName: String = "star", color: Color = .orange, showScore: Bool = true) {
+    public init(count: Int = 5, value: Binding<Double>, imageName: String = "star", color: Color = .orange, layout: LayoutType = .full) {
         self.count = count
         self._value = value
         self.imageName = imageName
         self.color = color
-        self.showScore = showScore
+        self.layout = layout
     }
     
     public var body: some View {
         VStack(spacing: 8) {
-            if showScore {
+            switch layout {
+            case .full:
                 Text("\(value, specifier: "%.1f")")
                     .fontDesign(.rounded)
                     .font(.largeTitle)
                     .fontWeight(.bold)
-            }
-            
-            ZStack {
-                ShapeRow(count: count, imageName: imageName, position: .background, color: color, value: $value)
-                ShapeRow(count: count, imageName: imageName, position: .foreground, color: color, value: $value)
+                
+                ZStack {
+                    ShapeRow(count: count, imageName: imageName, position: .background, color: color, value: $value)
+                    ShapeRow(count: count, imageName: imageName, position: .foreground, color: color, value: $value)
+                }
+            case .score:
+                Text("\(value, specifier: "%.1f")")
+                    .fontDesign(.rounded)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            case .graphical:
+                ZStack {
+                    ShapeRow(count: count, imageName: imageName, position: .background, color: color, value: $value)
+                    ShapeRow(count: count, imageName: imageName, position: .foreground, color: color, value: $value)
+                }
             }
         }
     }
