fd90b80b89fffad086d7ca300842630f9aa76394
Yohannes Haile <yohannes@kemercode.com>
2024-10-02 19:39:41 +0300

    deprecate ShapeProgressView

--- Sources/ReviewKit/ReviewKit.swift
+++ Sources/ReviewKit/ReviewKit.swift
@@ -15,6 +15,112 @@ public enum LayoutType {
     case full, score, graphical
 }
 
+@available(*, deprecated, renamed: "RatingView", message: "ShapeProgressView has been renamed to RatingView. ShapeProgressView will be removed in a future version.")
+public struct ShapeProgressView: View {
+    let appId: String
+    let count: Int
+    let imageName: String
+    let color: Color
+    let layout: LayoutType
+    let showReviewCount: Bool
+    let showLaurels: Bool
+    let countryCode: String
+    
+    @State private var reviewManager: ReviewManager
+    
+    public init(appId: String,
+                count: Int = 5,
+                imageName: String = "star",
+                color: Color = .orange,
+                layout: LayoutType = .full,
+                showReviewCount: Bool = true,
+                showLaurels: Bool = true,
+                countryCode: String = "US"
+    ) {
+        self.appId = appId
+        self.count = count
+        self.imageName = imageName
+        self.color = color
+        self.layout = layout
+        self.showReviewCount = showReviewCount
+        self.showLaurels = showLaurels
+        self.countryCode = countryCode
+        self._reviewManager = State(wrappedValue: ReviewManager(appId: appId, countryCode: countryCode))
+    }
+    
+    public var body: some View {
+        VStack(spacing: 8) {
+            switch layout {
+            case .full:
+                HStack {
+                    if showLaurels {
+                        Image(systemName: "laurel.leading")
+                    }
+
+                    Text("\(reviewManager.rating, specifier: "%.1f")")
+                        .fontDesign(.rounded)
+
+                    if showLaurels {
+                        Image(systemName: "laurel.trailing")
+                    }
+                }
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+                ZStack {
+                    StarRow(count: count, imageName: imageName, position: .background, color: color, value: $reviewManager.rating)
+                    StarRow(count: count, imageName: imageName, position: .foreground, color: color, value: $reviewManager.rating)
+                }
+                
+                if showReviewCount {
+                    Text(reviewManager.reviewCount > 0 ? "Based on \(reviewManager.reviewCount, specifier: "%.0f") reviews" : "No reviews yet")
+                }
+            case .score:
+                HStack {
+                    if showLaurels {
+                        Image(systemName: "laurel.leading")
+                    }
+
+                    Text("\(reviewManager.rating, specifier: "%.1f")")
+                        .fontDesign(.rounded)
+
+                    if showLaurels {
+                        Image(systemName: "laurel.trailing")
+                    }
+                }
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+                if showReviewCount {
+                    Text(reviewManager.reviewCount > 0 ? "Based on \(reviewManager.reviewCount, specifier: "%.0f") reviews" : "No reviews yet")
+                }
+            case .graphical:
+                ZStack {
+                    StarRow(count: count, imageName: imageName, position: .background, color: color, value: $reviewManager.rating)
+                    StarRow(count: count, imageName: imageName, position: .foreground, color: color, value: $reviewManager.rating)
+                }
+                
+                if showReviewCount {
+                    Text(reviewManager.reviewCount > 0 ? "Based on \(reviewManager.reviewCount, specifier: "%.0f") reviews" : "No reviews yet")
+                }
+            }
+        }
+        .task {
+            do {
+                try await reviewManager.fetchAppStoreRating()
+            } catch AppStoreResponseError.invalidData {
+                print("Trying to fetch App Store rating failed due to invalid data.")
+            } catch AppStoreResponseError.invalidURL {
+                print("Trying to fetch App Store rating failed due to invalid URL.")
+            } catch AppStoreResponseError.invalidResponse {
+                print("Trying to fetch App Store rating failed due to invalid response.")
+            } catch {
+                print("Trying to fetch App Store rating failed due to an unknown error.")
+            }
+        }
+    }
+}
+
 public struct RatingView: View {
     let appId: String
     let count: Int
